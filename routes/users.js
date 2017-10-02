181723ae27ee54b4978a3f6bafb444bfebfded2f
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-10-01 22:42:32 -0400

    Improvements on jsonformatter

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const JSONFormatter = require('../lib/JSONFormatter');
 /* GET users listing. */
 router.get('/', (req, res, next) => {
     userManager.getUsers()
-        .then(results => res.jsonp(JSONFormatter(results)))
+        .then(results => res.jsonp(JSONFormatter.format('users', results)))
         .catch(err => next(err));
 });
 
 router.get('/:id', (req, res, next) => {
     userManager.getUserById(req.params.id)
-        .then(results => res.jsonp(JSONFormatter(results)))
+        .then(results => res.jsonp(JSONFormatter.format('users', results)))
         .catch(err => next(err));
 });
 
